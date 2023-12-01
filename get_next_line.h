0147bff82220654d2b387afb418c24cdd636532b
Theo Paesch <tpaesch@1-D-6.42heilbronn.de>
2023-12-01 18:23:04 +0100

    header change

--- get_next_line.h
+++ get_next_line.h
@@ -6,14 +6,14 @@
 /*   By: tpaesch <tpaesch@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/23 12:01:04 by tpaesch           #+#    #+#             */
-/*   Updated: 2023/12/01 15:10:41 by tpaesch          ###   ########.fr       */
+/*   Updated: 2023/12/01 18:16:15 by tpaesch          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 # ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 10
+#  define BUFFER_SIZE 1
 # endif
 
 # include <fcntl.h>
@@ -22,12 +22,12 @@
 
 char	*get_next_line(int fd);
 
-char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strjoin(char *s1, char *s2);
 
-char	*ft_strchr(const char *s, int c);
+char	*ft_strchr(char *s, int c);
 
 void	*ft_calloc(size_t count, size_t size);
 
-size_t	ft_strlen(const char *str);
+size_t	ft_strlen(char *str);
 
 #endif
